30358c6757e68f4b0e4f56fcac6bb34fd8f4c178
niranjanwho <niranjanshettigar@hotmail.com>
2026-02-24 15:25:39 +0530

    added page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
@@ -7,23 +8,40 @@ import Contact from './components/Contact'
 import NotFound from './components/NotFound'
 import './App.css'
 
+// Define page animation
+const pageVariants = {
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 }
+}
+
 function App() {
+  const location = useLocation();
   return (
-    <Router>
     <>
       <Header />
       
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<PageTransition><Home /></PageTransition>} />
+          <Route path='/about' element={<PageTransition><About /></PageTransition>} />
+          <Route path='/contact' element={<PageTransition><Contact /></PageTransition>} />
+          <Route path='*' element={<PageTransition><NotFound /></PageTransition>} />
+        </Routes>
+      </AnimatePresence>
       
       <Footer />
     </>
-    </Router>
   )
+}
+
+// Animation wrapper 
+function PageTransition({ children }) {
+  return(
+    <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit" transition={{ duration: 0.3 }}>
+      {children}
+    </motion.div>
+  );
 }
 
 export default App
